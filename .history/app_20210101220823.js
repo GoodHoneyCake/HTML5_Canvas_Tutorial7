7e640cbd4ed6cad074b402aac1cd3e41ab9cff80
kim <beomsu9696@daum.net>
2021-01-01 22:12:08 +0900

    add html,css,js

---
+++ .history/app_20210101220823.js
@@ -0,0 +1,19 @@
+class App {
+  constructor() {
+    this.canvas = document.createElement("canvas");
+    this.ctx = this.canvas.getContext("2d");
+    document.body.appendChild(this.canvas);
+
+    this.pixelRatio = window.devicePixelRatio > 1 ? 2 : 1;
+
+    window.addEventListener("resize", this.resize.bind(this), false);
+    this.resize();
+  }
+
+  resize() {
+    this.stageWidth = document.body.clientWidth;
+    this.stageHeight = document.body.clientHeight;
+
+    this.canvas.width = this.stageWidth * this.pixelRatio;
+  }
+}
