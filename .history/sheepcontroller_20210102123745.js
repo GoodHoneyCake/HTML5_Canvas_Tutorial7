1a2d49528107e082d529226be82efe6702d7e5d9
kim <beomsu9696@daum.net>
2021-01-02 12:42:43 +0900

    add sheepcontroller.js

---
+++ .history/sheepcontroller_20210102123745.js
@@ -0,0 +1,11 @@
+export class SheepController {
+  constructor() {}
+  resize(stageWidth, stageHeight) {
+    this.stageWidth = stageWidth;
+    this.stageHeight = stageHeight;
+  }
+
+  loaded() {}
+  addSheep() {}
+  draw(ctx, t, dots) {}
+}
---
+++ .history/sheepcontroller_20210102124006.js
@@ -0,0 +1,30 @@
+export class SheepController {
+  constructor() {
+    this.img = new Image();
+    this.img.onload = () => {
+      this.loaded();
+    };
+    this.img.src = "sheep.png";
+
+    this.items = [];
+
+    this.cur = 0;
+    this.isLoaded = false;
+  }
+
+  resize(stageWidth, stageHeight) {
+    this.stageWidth = stageWidth;
+    this.stageHeight = stageHeight;
+  }
+
+  loaded() {
+    this.isLoaded = true;
+    this.addSheep();
+  }
+
+  addSheep() {
+    this.items.push(new Sheep(this.img, this.stageWidth));
+  }
+
+  draw(ctx, t, dots) {}
+}
